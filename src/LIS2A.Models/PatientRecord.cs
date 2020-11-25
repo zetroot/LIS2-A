e346ee946b1dda272ffe06dad14a93517b859586
zetroot <zetroot@gmail.com>
2020-11-25 18:09:30 +0300

    finished with fields of patient record

--- src/LIS2A.Models/PatientRecord.cs
+++ src/LIS2A.Models/PatientRecord.cs
@@ -7,11 +7,40 @@ namespace LIS2A.Models
     public struct PatientRecord
     {
         public RecordType RecordTypeID => RecordType.PatientIdentifying;
-        public int SequenceNumber { get; private set; }
-        public string PracticeID { get; private set; }
-        public string LaboratoryID { get; private set; }
-        public string ID3 { get; private set; }
-        public string Name { get; private set; }
-        public string MothersMaidenName { get; private set; }
+        public int SequenceNumber { get; }
+        public string PracticeID { get; }
+        public string LaboratoryID { get; }
+        public string ID3 { get; }
+        public string Name { get; }
+        public string MothersMaidenName { get; }
+        public DateTime Birthdate { get; }
+        public PatientSex Sex { get; } 
+        public string RaceEthnicOrigin { get; }
+        public string Address { get; }
+        public string Reserved0 { get; }
+        public string Telephone { get; }
+        public string PhysicianID { get; }
+        public string Special1 { get; }
+        public string Special2 { get; }
+        public string Height { get; }
+        public string Weight { get; }
+        public string Diagnosis { get; }
+        public string ActiveMedications { get; }
+        public string Diet { get; }
+        public string PracticeField1 { get; }
+        public string PracticeField2 { get; }
+        public string AdmissionDischarge { get; }
+        public string AdmissionStatus { get; }
+        public string Location { get; }
+        public string NatureOfAltDiagnosticCode { get; }
+        public string AltDiagnosticCode { get; }
+        public string Religion { get; }
+        public string MartialStatus { get; }
+        public string IsolationStatus { get; }
+        public string Language { get; }
+        public string HospitalService { get; }
+        public string HospitalInstitution { get; }
+        public string DosageCategory { get; }
+
     }
 }
---
+++ src/LIS2A.Models/PatientSex.cs
@@ -0,0 +1,29 @@
+﻿namespace LIS2A.Models
+{
+    public struct PatientSex
+    {
+        public static PatientSex Male = new PatientSex('M');
+        public static PatientSex Female = new PatientSex('F');
+        public static PatientSex Undefined = new PatientSex('U');
+
+        private readonly char sexChar;
+
+        private PatientSex(char sexChar)
+        {
+            this.sexChar = sexChar;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is PatientSex patientSex)
+                return this.sexChar == patientSex.sexChar;
+            return false;
+        }
+
+        public override int GetHashCode() => sexChar.GetHashCode();
+
+        public static bool operator ==(PatientSex left, PatientSex right) => left.sexChar == right.sexChar;
+
+        public static bool operator !=(PatientSex left, PatientSex right) => !(left == right);
+    }
+}
